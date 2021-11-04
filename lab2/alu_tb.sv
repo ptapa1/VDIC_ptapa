eaac934640185a420d401a0a38ee0e42b39178c6
ptapa <paulka107@wp.pl>
2021-11-04 16:19:41 +0100

    v1.3

--- lab2/alu_tb.sv
+++ lab2/alu_tb.sv
@@ -12,25 +12,24 @@ module top;
 		or_op                    = 3'b001,
 		add_op                   = 3'b100,
 		sub_op                   = 3'b101} operation_t;
-	
+
 	typedef enum bit[2:0] {no_op1 = 3'b010,
 		no_op2                    = 3'b011,
 		no_op3                   = 3'b110,
 		no_op4                   = 3'b111} no_ops;
-	
+
 	bit              sin = 1;
 	bit                 sout;
 	bit                clk;
-	bit                rst_n;
+	bit                rst_n=1'b1;
 
-	//string             test_result = "PASSED";
-	//string             all_tests_result = "PASSED";
-	
+	string             test_result = "PASSED";
+
 	bit [31:0] A,B,C;
 	bit [3:0] crc, flags;
-	bit [2:0] crc_out, crc_expected, op;
+	bit [2:0] op;
+	bit [2:0] crc_out;
 	bit [54:0] out;
-	bit [10:0] ctr=10'b0;
 	bit send_error_flag_data=0,send_error_flag_crc=0,send_error_flag_op=0;
 	bit [1:0] error_trig=2'b0;
 	bit done=1'b0;
@@ -54,11 +53,8 @@ module top;
 		option.name = "cg_op_cov";
 
 		coverpoint operation {
-			// #A1 test all operations
-			bins test_all_ops[] = {[and_op : sub_op]};
-
-		// #A6 two operations in row
-		//?? bins A6_twoops[]       = ([and_op : sub_op] [* 2]);
+			
+			bins A1_all_ops[] = {[and_op : sub_op]};
 
 		}
 
@@ -70,49 +66,44 @@ module top;
 		option.name = "cg_zeros_or_ones_on_ops";
 
 		all_ops : coverpoint operation {
-			//bins test_all_ops[] = {[and_op : sub_op]};
 		}
 
 		a_leg: coverpoint A {
-	        bins zeros = {'h00000000};
-	        bins others= {['h01:'hFFFFFFFE]};
-	        bins ones  = {'hFFFFFFFF};
-	    }
-	
-	    b_leg: coverpoint B {
-	        bins zeros = {'h00000000};
-	        bins others= {['h01:'hFFFFFFFE]};
-	        bins ones  = {'hFFFFFFFF};
-	    }
+			bins zeros = {'h00000000};
+			bins others= {['h01:'hFFFFFFFE]};
+			bins ones  = {'hFFFFFFFF};
+		}
+
+		b_leg: coverpoint B {
+			bins zeros = {'h00000000};
+			bins others= {['h01:'hFFFFFFFE]};
+			bins ones  = {'hFFFFFFFF};
+		}
 
 		B_op_00_FF: cross a_leg, b_leg, all_ops {
 
-			// #B1 simulate all zero input for all the operations
-
-			bins test_zeros_add_00          = binsof (all_ops) intersect {add_op} &&
+			bins A2_zeros_add_00          = binsof (all_ops) intersect {add_op} &&
 			(binsof (a_leg.zeros) || binsof (b_leg.zeros));
 
-			bins test_zeros_and_00          = binsof (all_ops) intersect {and_op} &&
+			bins A2_zeros_and_00          = binsof (all_ops) intersect {and_op} &&
 			(binsof (a_leg.zeros) || binsof (b_leg.zeros));
 
-			bins test_zeros_or_00          = binsof (all_ops) intersect {or_op} &&
+			bins A2_zeros_or_00          = binsof (all_ops) intersect {or_op} &&
 			(binsof (a_leg.zeros) || binsof (b_leg.zeros));
 
-			bins test_zeros_sub_00          = binsof (all_ops) intersect {sub_op} &&
+			bins A2_zeros_sub_00          = binsof (all_ops) intersect {sub_op} &&
 			(binsof (a_leg.zeros) || binsof (b_leg.zeros));
 
-			// #B2 simulate all one input for all the operations
-
-			bins test_ones_add_FF          = binsof (all_ops) intersect {add_op} &&
+			bins A2_ones_add_FF          = binsof (all_ops) intersect {add_op} &&
 			(binsof (a_leg.ones) || binsof (b_leg.ones));
 
-			bins test_ones_and_FF          = binsof (all_ops) intersect {and_op} &&
+			bins A2_ones_and_FF          = binsof (all_ops) intersect {and_op} &&
 			(binsof (a_leg.ones) || binsof (b_leg.ones));
 
-			bins test_ones_or_FF          = binsof (all_ops) intersect {or_op} &&
+			bins A2_ones_or_FF          = binsof (all_ops) intersect {or_op} &&
 			(binsof (a_leg.ones) || binsof (b_leg.ones));
 
-			bins test_ones_sub_FF          = binsof (all_ops) intersect {sub_op} &&
+			bins A2_ones_sub_FF          = binsof (all_ops) intersect {sub_op} &&
 			(binsof (a_leg.ones) || binsof (b_leg.ones));
 
 
@@ -127,46 +118,46 @@ module top;
 		option.name = "cg_output_flags";
 
 		coverpoint flags {
-			bins carry = {'h8};
-			bins overflow= {'h4};
-			bins zero  = {'h2};
-			bins negative  = {'h1};
+			bins A3_flag_carry = {'h8};
+			bins A3_flag_overflow= {'h4};
+			bins A3_flag_zero  = {'h2};
+			bins A3_flag_negative  = {'h1};
 		}
 
 	endgroup
-	
+
 	covergroup err_flags;
 
 		option.name = "cg_error_flags";
 
 		coverpoint error_flag {
-			bins err_data = {'h24};
-			bins err_crc= {'h12};
-			bins err_op  = {'h9};
+			bins A4_err_flag_data = {'h24};
+			bins A4_err_flag_crc= {'h12};
+			bins A4_err_flag_op  = {'h9};
 		}
 
 	endgroup
 
-op_cov                      oc;
-zeros_or_ones_on_ops        c_00_FF;
-output_flags				out_flags;
-err_flags					flags_errors;
+	op_cov                      oc;
+	zeros_or_ones_on_ops        c_00_FF;
+	output_flags                out_flags;
+	err_flags                   flags_errors;
 
-initial begin : coverage
-    oc      = new();
-    c_00_FF = new();
-	out_flags = new();
-	flags_errors = new();
-    forever begin : sample_cov
-        @(posedge clk);
-        if(rst_n) begin
-            oc.sample();
-            c_00_FF.sample();
-	        out_flags.sample();
-	        flags_errors.sample();
-        end
-    end
-end : coverage
+	initial begin : coverage
+		oc      = new();
+		c_00_FF = new();
+		out_flags = new();
+		flags_errors = new();
+		forever begin : sample_cov
+			@(posedge clk);
+			if( done || !rst_n) begin
+				oc.sample();
+				c_00_FF.sample();
+				out_flags.sample();
+				flags_errors.sample();
+			end
+		end
+	end : coverage
 
 //------------------------------------------------------------------------------
 // Clock generator
@@ -223,13 +214,13 @@ end : coverage
 
 //---------------------------------
 	function bit [1:0] trigger_error();
-		bit [11:0] error;
-		error = 12'($random);
-		if (error == 12'h111)
+		bit [1:0] error;
+		error = 2'($random);
+		if (error == 2'b01)
 			return 2'b01;
-		else if (error == 12'h222)
+		else if (error == 2'b10)
 			return 2'b10;
-		else if (error == 12'h444)
+		else if (error == 2'b11)
 			return 2'b11;
 		else
 			return 2'b00;
@@ -237,102 +228,92 @@ end : coverage
 //------------------------
 // Tester main
 
-	
 
 	initial begin : tester
 		reset_alu();
-		ctr=10'b0;
-		repeat(100000)begin
-			
-	    
-	        @(negedge clk);                                                                                                                                 
-	        operation = get_op();                                                                                                                           
-	        A      = get_data();
+		repeat(10000)begin
+			@(negedge clk);
+			operation = get_op();
+			A      = get_data();
 			B      = get_data();
-			crc = get_crc(B,A,operation);                                                                                                                     
+			crc = get_crc(B,A,operation);
 			error_trig = trigger_error();
 			if(error_trig == 2'b01) begin//
 				send_error_flag_data <= 1'b1;
-				send_data(B[31:24]);                                                                                            
+				send_data(B[31:24]);
 				send_data(B[23:16]);
-				
+
 				send_data(B[7:0]);
-				
-				send_data(A[23:16]);                                                                                     
-		        send_data(A[15:8]);                                                                                      
-				send_data(A[7:0]);                                                                                       
+
+				send_data(A[23:16]);
+				send_data(A[15:8]);
+				send_data(A[7:0]);
 				send_command(operation,crc);
-				$display("data_error, flag=%b", send_error_flag_data);
 			end
-			
+
 			else if(error_trig == 2'b10) begin
 				send_error_flag_crc <= 1'b1;
 				crc = crc + 2'($random);
-				send_data(B[31:24]);                                                                                            
-				send_data(B[23:16]);                                                                                     
-				send_data(B[15:8]);                                                                                      
-				send_data(B[7:0]);                                                                                       
-				send_data(A[31:24]);                                                                                     
-				send_data(A[23:16]);                                                                                     
-		        send_data(A[15:8]);                                                                                      
-				send_data(A[7:0]);                                                                                       
+				send_data(B[31:24]);
+				send_data(B[23:16]);
+				send_data(B[15:8]);
+				send_data(B[7:0]);
+				send_data(A[31:24]);
+				send_data(A[23:16]);
+				send_data(A[15:8]);
+				send_data(A[7:0]);
 				send_command(operation,crc);
-				$display("crc_error, flag=%b", send_error_flag_crc);
 			end
-			
+
 			else if(error_trig == 2'b11) begin
 				send_error_flag_op <= 1'b1;
 				op = get_no_op();
 				crc = get_crc(B,A,op);
-				send_data(B[31:24]);                                                                                            
-				send_data(B[23:16]);                                                                                     
-				send_data(B[15:8]);                                                                                      
-				send_data(B[7:0]);                                                                                       
-				send_data(A[31:24]);                                                                                     
-				send_data(A[23:16]);                                                                                     
-		        send_data(A[15:8]);                                                                                      
-				send_data(A[7:0]);                                                                                       
+				send_data(B[31:24]);
+				send_data(B[23:16]);
+				send_data(B[15:8]);
+				send_data(B[7:0]);
+				send_data(A[31:24]);
+				send_data(A[23:16]);
+				send_data(A[15:8]);
+				send_data(A[7:0]);
 				send_command(op,crc);
-				$display("op_error, flag=%b", send_error_flag_op);
+
 			end
-			
+
 			else begin
-				send_data(B[31:24]);                                                                                            
-				send_data(B[23:16]);                                                                                     
-				send_data(B[15:8]);                                                                                      
-				send_data(B[7:0]);                                                                                       
-				send_data(A[31:24]);                                                                                     
-				send_data(A[23:16]);                                                                                     
-		        send_data(A[15:8]);                                                                                      
-				send_data(A[7:0]);                                                                                       
+				send_data(B[31:24]);
+				send_data(B[23:16]);
+				send_data(B[15:8]);
+				send_data(B[7:0]);
+				send_data(A[31:24]);
+				send_data(A[23:16]);
+				send_data(A[15:8]);
+				send_data(A[7:0]);
 				send_command(operation,crc);
-				//$display("no error");
-			end 
-			
+			end
+
 			read_data(out);
-			
-			if(out[54:53] == 2'b00)begin                                                                             
-				process_data(out[54:44],C[31:24]);                                                                   
-				process_data(out[43:33],C[23:16]);                                                                   
-				process_data(out[32:22],C[15:8]);                                                                    
-				process_data(out[21:11],C[7:0]);                                                                     
-				process_command(out[10:0],flags,crc_out);                                                            
-				crc_expected = get_crc_out(C,flags);                                                                   
-			end                                                                                                      
-			else if(out[54:53] == 2'b01)begin                                                                        
-				process_error(out[54:44],error_flag);                                                                                                                                                                  
-			end                                                                                                      
-			else begin                                                                                               
-				$display("INTERNAL ERROR - incorrect packet returned\n");                                                                                   
+
+			if(out[54:53] == 2'b00)begin
+				process_data(out[54:44],C[31:24]);
+				process_data(out[43:33],C[23:16]);
+				process_data(out[32:22],C[15:8]);
+				process_data(out[21:11],C[7:0]);
+				process_command(out[10:0],flags,crc_out);
+			end
+			else if(out[54:53] == 2'b01)begin
+				process_error(out[54:44],error_flag);
+			end
+			else begin
+				$display("INTERNAL ERROR - incorrect packet returned\n");
+				test_result = "FAILED";
 			end
 			done = 1'b1;
-			ctr = ctr+10'b1;
-		    if($get_coverage() == 100) break; 
-		    
-		end                                  	
-	
-	
-	
+			if($get_coverage() == 100) break;
+
+		end
+
 		$finish;
 	end : tester
 
@@ -343,6 +324,7 @@ end : coverage
 	`ifdef DEBUG
 		$display("%0t DEBUG: reset_alu", $time);
 	`endif
+		@(negedge clk);
 		rst_n = 1'b0;
 		@(negedge clk);
 		rst_n = 1'b1;
@@ -375,19 +357,7 @@ end : coverage
 			sin = packet[i];
 		end
 	endtask
-	
-	task send_message;
-		input[2:0] in_op;
-		input[3:0] in_crc;
-		static reg [10:0] packet = 11'b01000000001;
 
-		packet [7:5]= in_op;
-		packet [4:1]= in_crc;
-		for(int i=10;i>=0;i--)begin
-			@(negedge clk);
-			sin = packet[i];
-		end
-	endtask
 
 //------------------------------------------------------------------------------
 // read data and command tasks
@@ -442,8 +412,8 @@ end : coverage
 		);
 		bit [31:0] ret;
 		`ifdef DEBUG
-	    $display("%0t DEBUG: get_expected(%0d,%0d,%0d)",$time, A, B, op_set);
-	    `endif
+		$display("%0t DEBUG: get_expected(%0d,%0d,%0d)",$time, A, B, op_set);
+		`endif
 		case(op_set)
 			and_op : ret = A & B;
 			add_op : ret = A + B;
@@ -485,59 +455,43 @@ end : coverage
 	endfunction
 
 
-	function [2:0] get_crc_out;
-		input [31:0] data;
-		input [3:0] flags;
-		reg [36:0] d;
-		reg [2:0] c;
-		reg [2:0] newcrc;
-		begin
-			d [36:5]= data;
-			d [4]= 1'b0;
-			d [3:0]= flags;
-			c = 3'b000;
-
-			newcrc[0] = d[35] ^ d[32] ^ d[31] ^ d[30] ^ d[28] ^ d[25] ^ d[24] ^ d[23] ^ d[21] ^ d[18] ^ d[17] ^ d[16] ^ d[14] ^ d[11] ^ d[10] ^ d[9] ^ d[7] ^ d[4] ^ d[3] ^ d[2] ^ d[0] ^ c[1];
-			newcrc[1] = d[36] ^ d[35] ^ d[33] ^ d[30] ^ d[29] ^ d[28] ^ d[26] ^ d[23] ^ d[22] ^ d[21] ^ d[19] ^ d[16] ^ d[15] ^ d[14] ^ d[12] ^ d[9] ^ d[8] ^ d[7] ^ d[5] ^ d[2] ^ d[1] ^ d[0] ^ c[1] ^ c[2];
-			newcrc[2] = d[36] ^ d[34] ^ d[31] ^ d[30] ^ d[29] ^ d[27] ^ d[24] ^ d[23] ^ d[22] ^ d[20] ^ d[17] ^ d[16] ^ d[15] ^ d[13] ^ d[10] ^ d[9] ^ d[8] ^ d[6] ^ d[3] ^ d[2] ^ d[1] ^ c[0] ^ c[2];
-			get_crc_out = newcrc;
-		end
-
-
-	endfunction
 
 //------------------------------------------------------------------------------
 // Scoreboard
 //------------------------------------------------------------------------------
-always @(negedge clk) begin : scoreboard
-    if(done) begin:verify_result
-        
-        if(send_error_flag_data || send_error_flag_crc || send_error_flag_op) begin
-        	`ifdef DEBUG
-            $display("%0t Expected error packet for flag %s received for A=%0d B=%0d op_set=%0d", $time, error_flag.name, A, B, operation);
-           `endif
-        end
-        else begin
-	        logic [31:0] predicted_result;
-	
-	        predicted_result = get_expected(A, B, operation);
-	        
-	        CHK_RESULT: assert(C === predicted_result) begin
-	           `ifdef DEBUG
-	            $display("%0t Test passed for A=%0d B=%0d op_set=%0d", $time, A, B, operation);
-	           `endif
-	        end
-	        else begin
-	        	$warning("%0t Test FAILED for A=%0d B=%0d op_set=%0d\nExpected: %d  received: %d",
-	            $time, A, B, operation , predicted_result, C);
-    		end;
-	    end
-	    done <= 1'b0;
-	    send_error_flag_data <= 1'b0;
-	    send_error_flag_crc <= 1'b0;
-	    send_error_flag_op <= 1'b0;
-    end 
-    
-end : scoreboard
+	always @(negedge clk) begin : scoreboard
+		if(done) begin:verify_result
 
+			if(send_error_flag_data || send_error_flag_crc || send_error_flag_op) begin
+			`ifdef DEBUG
+				$display("%0t Expected error packet for flag %s received for A=%0d B=%0d op_set=%0d", $time, error_flag.name, A, B, operation);
+		   `endif
+			end
+			else begin
+				logic [31:0] predicted_result;
+
+				predicted_result = get_expected(A, B, operation);
+
+				CHK_RESULT: assert(C === predicted_result) begin
+			   `ifdef DEBUG
+					$display("%0t Test passed for A=%0d B=%0d op_set=%0d", $time, A, B, operation);
+			   `endif
+				end
+				else begin
+					$warning("%0t Test FAILED for A=%0d B=%0d op_set=%0d\nExpected: %d  received: %d",
+						$time, A, B, operation , predicted_result, C);
+						test_result <= "FAILED";
+				end;
+			end
+			done <= 1'b0;
+			send_error_flag_data <= 1'b0;
+			send_error_flag_crc <= 1'b0;
+			send_error_flag_op <= 1'b0;
+		end
+
+	end : scoreboard
+
+final begin
+	$display("Test %s", test_result);
+end
 endmodule
